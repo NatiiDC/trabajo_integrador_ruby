2e88e4ffb7ea081e13cf9ef5cac010f77720c88e
NatiiDC <nataliaeaparicio@gmail.com>
2015-12-08 12:16:43 -0300

    Agregue a la entidad persona

--- learn-rails/db/schema.rb
+++ learn-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207221653) do
+ActiveRecord::Schema.define(version: 20151208135717) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "firstname"
@@ -49,5 +49,12 @@ ActiveRecord::Schema.define(version: 20151207221653) do
 
   add_index "invoices", ["client_id"], name: "index_invoices_on_client_id"
   add_index "invoices", ["person_id"], name: "index_invoices_on_person_id"
+
+  create_table "people", force: :cascade do |t|
+    t.string   "name"
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
