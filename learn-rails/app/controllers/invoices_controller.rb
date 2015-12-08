b4c8520ae6b92eb9d60a62f385dfccd2f0b87c4e
NatiiDC <nataliaeaparicio@gmail.com>
2015-12-08 17:55:07 -0300

    No redirige bien el show y el index no anda

--- learn-rails/app/controllers/invoices_controller.rb
+++ learn-rails/app/controllers/invoices_controller.rb
@@ -1,70 +1,66 @@
 class InvoicesController < ApplicationController
+
   before_action :set_invoice, only: [:show, :edit, :update, :destroy]
 
   # GET /invoices
-  # GET /invoices.json
   def index
     @invoices = Invoice.all
   end
 
   # GET /invoices/1
-  # GET /invoices/1.json
   def show
+    @person = Person.find(@invoice.person_id)
   end
 
   # GET /invoices/new
   def new
+    @client = Client.find(params[:client_id])
     @invoice = Invoice.new
+    @people = Person.all
   end
 
   # GET /invoices/1/edit
   def edit
+    @people = Person.all
   end
 
   # POST /invoices
-  # POST /invoices.json
   def create
-    @invoice = Invoice.new(invoice_params)
+    @client = Client.find(params[:client_id])
+    @invoice = @client.invoices.new(invoice_params)
 
-    respond_to do |format|
       if @invoice.save
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully created.' }
-        format.json { render :show, status: :created, location: @invoice }
+        redirect_to @client, notice: 'La factura ha sido creada '
       else
         format.html { render :new }
-        format.json { render json: @invoice.errors, status: :unprocessable_entity }
       end
-    end
   end
 
   # PATCH/PUT /invoices/1
-  # PATCH/PUT /invoices/1.json
   def update
     respond_to do |format|
       if @invoice.update(invoice_params)
-        format.html { redirect_to @invoice, notice: 'Invoice was successfully updated.' }
-        format.json { render :show, status: :ok, location: @invoice }
+        format.html { redirect_to @invoice, notice: 'La factura ha sido actualizada correctamente.' }
       else
         format.html { render :edit }
-        format.json { render json: @invoice.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /invoices/1
-  # DELETE /invoices/1.json
   def destroy
     @invoice.destroy
     respond_to do |format|
-      format.html { redirect_to invoices_url, notice: 'Invoice was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to invoices_url, notice: 'La factura ha sido borrada correctamente.' }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_invoice
-      @invoice = Invoice.find(params[:id])
+      # @invoice = Invoice.find(params[:id])
+      @client = Client.find(params[:client_id])
+      @invoice = @client.invoices.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
