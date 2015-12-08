30f2d4c944b6a15021d37afd2290b2bd9f04c673
NatiiDC <nataliaeaparicio@gmail.com>
2015-12-08 17:42:13 -0300

    mensajes en español

--- learn-rails/app/controllers/people_controller.rb
+++ learn-rails/app/controllers/people_controller.rb
@@ -28,36 +28,29 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       if @person.save
-        format.html { redirect_to @person, notice: 'Person was successfully created.' }
-        format.json { render :show, status: :created, location: @person }
+        format.html { redirect_to @person, notice: 'La persona ha sido creada exitosamente.' }
       else
         format.html { render :new }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /people/1
-  # PATCH/PUT /people/1.json
   def update
     respond_to do |format|
       if @person.update(person_params)
-        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { render :show, status: :ok, location: @person }
+        format.html { redirect_to @person, notice: 'La persona ha sido actualizada exitosamente.' }
       else
         format.html { render :edit }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /people/1
-  # DELETE /people/1.json
   def destroy
     @person.destroy
     respond_to do |format|
-      format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to people_url, notice: 'La persona ha sido eliminada.' }
     end
   end
 
