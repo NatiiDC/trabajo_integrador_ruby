b340943eb8aaced00a64e22a487194bf041afc5b
MagoDopado <nataliaeaparicio@gmail.com>
2015-12-21 12:21:52 -0300

    Se agregaron validaciones en el modelos Invoice

--- learn-rails/app/models/invoice.rb
+++ learn-rails/app/models/invoice.rb
@@ -6,4 +6,21 @@ class Invoice < ActiveRecord::Base
   paginates_per 10
   max_paginates_per 50
 
+validates :description,
+  presence: true,
+  length: { maximum: 200 }
+validates :amount,
+  presence: true,
+  numericality: {
+    greater_than: 0,
+    less_than_or_equal_to: 1000000000
+  }
+validates :date_issue,
+  presence: true
+validates :client_id,
+  presence: true
+validates :person_id,
+  presence: true
+
+
 end
