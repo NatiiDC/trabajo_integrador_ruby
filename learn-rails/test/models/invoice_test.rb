d21d5f5c9ac407d5ae3a9d9dca8aae4f6f9e6b6e
MagoDopado <nataliaeaparicio@gmail.com>
2015-12-21 12:23:54 -0300

    Test de facturas hecho

--- learn-rails/test/models/invoice_test.rb
+++ learn-rails/test/models/invoice_test.rb
@@ -15,33 +15,28 @@ class InvoiceTest < ActiveSupport::TestCase
   test "Nueva factura con errores de validación" do
 
     assert_not Invoice.new(
-    amount:" 123.5", date_issue:" 2014-5-21",
-    client_id:" client1", person_id:" person1"
-    ).save, "Guardando una factura sin descripción"
+    amount: "123.5", date_issue: "2014-5-21",
+    client_id: "client1", person_id: "person1").save, "Guardando una factura sin descripción"
 
     assert_not Invoice.new(
-    description: "Una descripcion cualquiera"
-    date_issue:" 2014-5-21",
-    client_id:" client1", person_id:" person1"
-    ).save, "Guardando una factura sin monto"
+    description: "Una descripcion cualquiera",
+    date_issue: "2014-5-21",
+    client_id: "client1", person_id: "person1").save, "Guardando una factura sin monto"
 
     assert_not Invoice.new(
-    description: "Una descripcion cualquiera"
-    amount:" 123.5",
-    client_id:" client1", person_id:" person1"
-    ).save, "Guardando una factura sin el dia de facturación"
+    description: "Una descripcion cualquiera",
+    amount: "123.5",
+    client_id: "client1", person_id: "person1").save, "Guardando una factura sin el dia de facturación"
 
     assert_not Invoice.new(
-    description: "Una descripcion cualquiera"
-    amount:" 123.5", date_issue:" 2014-5-21",
-    person_id:" person1"
-    ).save, "Guardando una factura sin el id del cliente"
+    description: "Una descripcion cualquiera",
+    amount: "123.5", date_issue: "2014-5-21",
+    person_id: "person1").save, "Guardando una factura sin el id del cliente"
 
     assert_not Invoice.new(
-    description: "Una descripcion cualquiera"
-    amount:" 123.5", date_issue:" 2014-5-21",
-    client_id:" client1",
-    ).save, "Guardando una factura sin el id de la persona"
+    description: "Una descripcion cualquiera",
+    amount: "123.5", date_issue: "2014-5-21",
+    client_id: "client1").save, "Guardando una factura sin el id de la persona"
 
   end
 
