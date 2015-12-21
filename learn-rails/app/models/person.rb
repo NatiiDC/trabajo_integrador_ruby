42aba9f0f999868e8e588cf9cb006351536f8e8b
MagoDopado <nataliaeaparicio@gmail.com>
2015-12-21 12:23:34 -0300

    Se agregaron validaciones en el modelo Person

--- learn-rails/app/models/person.rb
+++ learn-rails/app/models/person.rb
@@ -2,4 +2,12 @@ class Person < ActiveRecord::Base
 
   has_many :invoices, dependent: :restrict_with_error
 
+  validates :name,
+    presence: true,
+    length: { in: 2..20 }
+  validates :code,
+    format: { with: /\A[0-9]{2}-[0-9]{8}-[0-9]\Z/ },
+    presence: true,
+    uniqueness: true
+
 end
