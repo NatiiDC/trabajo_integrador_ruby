28b73e6f9e8343ad7ca85c33b7a263845d9cdd9c
MagoDopado <nataliaeaparicio@gmail.com>
2015-12-21 11:34:50 -0300

    Test hechos

--- learn-rails/test/models/client_test.rb
+++ learn-rails/test/models/client_test.rb
@@ -11,12 +11,6 @@ class ClientTest < ActiveSupport::TestCase
 #     Con alguna factura emitida
 
 
-  def setup
-    @clientWithInvoices = clients(:client1)
-    @clientWithOutInvoices = clients(:clientWithOutInvoices)
-  end
-
-
   test "Creacion de un nuevo Cliente sin algunos datos datos" do
     assert_not Client.new.save, "Guardando un cliente sin datos"
   end
@@ -28,61 +22,60 @@ class ClientTest < ActiveSupport::TestCase
     address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente repetido"
   end
 
-  test "Creacion de un nuevo Cliente con datos invalidos o sin todos los datos requeridos" do
+  test "Creacion de un nuevo Cliente con datos inválidos o sin todos los datos requeridos" do
 
     assert_not Client.new(
-    born: "1995-5-21",type_document_cd: "DNI",
-    number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
-    address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin nombre ni apellido"
+      born: "1995-5-21",type_document_cd: "DNI",
+      number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
+      address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin nombre ni apellido"
 
     assert_not Client.new(
-    firstname: "Natalia Elizabeth", lastname: "Aparicio",type_document_cd: "DNI",
-    number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
-    address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin fecha de nacimiento"
+      firstname: "Natalia Elizabeth", lastname: "Aparicio",type_document_cd: "DNI",
+      number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
+      address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin fecha de nacimiento"
 
     assert_not Client.new(
-    firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",
-    email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
-    address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin Documento"
+      firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",
+      email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
+      address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin Documento"
 
     assert_not Client.new(
-    firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
-    number_document: "38706162", phone: "+54-221-12345678",
-    address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin email"
+      firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
+      number_document: "38706162", phone: "+54-221-12345678",
+      address: "520 nro.1234", code: "20-38706162-7").save, "Guardando un cliente sin email"
 
     assert_not Client.new(
-    firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
-    number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
-    code: "20-38706162-7").save, "Guardando un cliente sin dirección"
+      firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
+      number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
+      code: "20-38706162-7").save, "Guardando un cliente sin dirección"
 
     assert_not Client.new(
-    firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
-    number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
-    address: "520 nro.1234").save, "Guardando un cliente sin CUIL/CUIT"
+      firstname: "Natalia Elizabeth", lastname: "Aparicio", born: "1995-5-21",type_document_cd: "DNI",
+      number_document: "38706162", email: "nataliaa@ejemplo.com", phone: "+54-221-12345678",
+      address: "520 nro.1234").save, "Guardando un cliente sin CUIL/CUIT"
 
   end
 
-  test "Creacion de un nuevo Cliente con datos validos" do
-    obj =  Client.new(
-      firstname: "Sara", lastname: "Kay", born: "1915-5-21", type_document_cd: "DNI",
-      number_document: "8706162", email: "sarakay@ejemplo.com",
-      address: "en algun lugar", code: "20-44321123-9"
-    )
-    assert obj.save, "Guardando un cliente con datos validos sin telefono"
+  test "Creacion de un nuevo Cliente con datos válidos" do
 
-    assert Client.new(
-    firstname: "Sarasa", lastname: "Tomate", born: "1915-5-21", type_document_cd: "LD",
-    number_document: "8706162", email: "sarasat@ejemplo.com",phone: "+54-221-8374931",
-    address: "en algun lugar", code: "20-12312134-8").save, "Guardando un cliente con datos validos con telefono"
+    Client.all.each do |cl|
+      if (! cl.phone.nil?)
+        assert cl.save, "Guardando un cliente con datos válidos con telefono"
+      else
+        assert cl.save, "Guardando un cliente con datos válidos sin telefono"
+      end
+    end
 
   end
 
-  test "Borrado de un cliente sin facturas" do
-    assert @clientWithOutInvoices.destroy(), "Borrando cliente sin facturas."
-  end
-
-  test "Borrado con un cliente con facturas" do
-    assert_not @clientWithInvoices.destroy(), "Borrando cliente con facturas."
-  end
+  test "Borrando clientes" do
+      Client.all.each do |cl|
+        if (cl.invoices.count == 0)
+          assert cl.destroy(), "Cliente sin facturas"
+        else
+          assert_not cl.destroy(), "Cliente con factura/s"
+        end
+      end
+    end
 
 end
